10c49083a82208417ec996587a8a59acfc4b223f
Ali Aziz <aliaziz@Alis-MacBook-Pro.local>
2018-09-12 14:14:51 +0100

    nothing

--- src/app.js
+++ src/app.js
@@ -1,13 +1,13 @@
 class App {
 
-  addListToPage(list) {
+  addListToPage (list) {
     list.forEach(item => {
-      const newItem = new Item(item);
-    mainList.innerHTML += newItem.renderListItem();
-    });
+      const newItem = new Item(item)
+      mainList.innerHTML += newItem.renderListItem()
+    })
   }
 
-  addCategory(categorySelect) {
+  addCategory (categorySelect) {
     let listItem = "<option value=''>--Please choose an option--</option>"
     Item.all.forEach(item => {
       listItem += `<option value="${item.category.id}">${item.category.name}</option>`
@@ -15,13 +15,13 @@ class App {
     categorySelect.innerHTML = listItem
   }
 
-  addCategoryMenu(theId, categories) {
+  addCategoryMenu (theId, categories) {
     categories.forEach(category => {
       theId.innerHTML += `<li id="category-${category.id}"><a>${category.name}</a></li>`
     })
     categories.forEach(category => {
       document.querySelector(`#category-${category.id}`).addEventListener('click', event => {
-        mainList.innerHTML = ""
+        mainList.innerHTML = ''
         let filter = Item.all
         let filteredItems = filter.filter(item => item.category.id === category.id)
         this.addListToPage(filteredItems)
@@ -29,15 +29,15 @@ class App {
     })
   }
 
-  addNewItemListener(form){
+  addNewItemListener (form) {
     form.addEventListener('submit', event => {
       event.preventDefault()
       Item.postNewItem({
-          user_id: user.id,
-          color: form.elements.color.value,
-          brand: form.elements.brand.value,
-          image: form.elements.image.value,
-          category_id: form.elements.category.value
+        user_id: user.id,
+        color: form.elements.color.value,
+        brand: form.elements.brand.value,
+        image: form.elements.image.value,
+        category_id: form.elements.category.value
       })
       .then(res =>
         // hide the form,
@@ -46,36 +46,23 @@ class App {
       .then(json => {
         console.log(json)
       })
+    })
+  }
+
+  addColorMenu (theId, colors) {
+    colors.forEach(color => {
+      theId.innerHTML += `<li id="color${item.color}"><a>${item.color}</a></li>`
+    })
+    colors.forEach(color => {
+      document.querySelector(`#color-${item.id}`).addEventListener('click', event => {
+        mainList.innerHTML = ''
+        let filter = Item.all
+        let filteredItems = filter.filter(color => color.id === item.id)
+        this.addListToPage(filteredItems)
       })
-    }
-
-
-    addColorMenu(theId, colors) {
-      colors.forEach(color => {
-        theId.innerHTML += `<li id="color${item.color}"><a>${item.color}</a></li>`
-      })
-      colors.forEach(color => {
-        document.querySelector(`#color-${item.id}`).addEventListener('click', event => {
-          mainList.innerHTML = ""
-          let filter = Item.all
-          let filteredItems = filter.filter(color => color.id === item.id)
-          this.addListToPage(filteredItems)
-        })
-      })
-    }
-
-
-
-
-
-
-
-
-
-
+    })
+  }
 
 }
-
-
 
 $(document).foundation()
